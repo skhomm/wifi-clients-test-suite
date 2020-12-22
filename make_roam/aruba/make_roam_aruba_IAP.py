78b027989ffb8c52fa3e648ef9170c4671d96679
skhomm <62881270+skhomm@users.noreply.github.com>
2020-12-22 14:36:44 +0300

    Feature/password handling (#1)
    * Delete spaces in print ()
    
    * Collect password using getpass()
    
    * Make cosmetic changes after enabling linter. Ooops.
    
    * Add even more cosmetics
    
    * Now devices database lives in external file: devices.json
    
    * Define and use function for each round of power changing
    
    * More elegant way to print output from show_command
    
    * Now commands are generated inside functions
    
    * Delete 2 files
    
    * Program body is not separated by functions and variables anymore
    
    * Add if __name__ == __main__
    
    Co-authored-by: skhomm <i.kwibus@gmail.com>

--- make_roam/aruba/make_roam_aruba_IAP.py
+++ make_roam/aruba/make_roam_aruba_IAP.py
@@ -1,80 +1,94 @@
-# Devices: Aruba IAP 
-# Firmware: 8.6.0.4
+"""
+This script creates conditions that drive a Wi-Fi client to roam.
+
+It's achieved by controlling power settings of APs.
+Client doesn't need to move. It roams from AP with low power.
+"""
 
 import time
+import json
+import getpass
+
 from netmiko import ConnectHandler
 
-# Define variables
-replay = 2
-min_power = 2
-max_power = 21
-step_power = -3
-countdown = 10 
-show_comand = "show ap debug driver-config | inc EIRP|Channel|BSSID"
-channel = {1: 149, 2: 157}
+# Define parameters
+REPLAY = 2
+MIN_POWER = 2
+MAX_POWER = 21
+STEP_POWER = -3
+COUNTDOWN = 10
+SHOW_ARGS = "EIRP|Channel|BSSID"
+CHANNEL = {1: 149, 2: 154}
 
-# Define devices and login credentials
-aruba_1 = {
-    'device_type': 'aruba_os',
-    'host':   '10.0.0.1',
-    'username': 'LOGIN',
-    'password': 'PASSWORD',
-}
-aruba_2 = {
-    'device_type': 'aruba_os',
-    'host':   '10.0.0.2',
-    'username': 'LOGIN',
-    'password': 'PASSWORD',
-}
 
-print ("It's roaming time!")
+def show_cmd(include_args):
+    return(f"show ap debug driver-config | inc {include_args}")
 
-for n in range(replay):
-    print ("\nRound",n+1)
-    
-    # One full round starts here
-    ap_counter=1
-    for device in (aruba_1, aruba_2):
-        print ("\n================================================================")
-        print ("Connecting to AP",ap_counter,"...")
-        net_connect = ConnectHandler(**device)
-        print ("Successful!")
-        
-        # Change power - step by step 
-        for tx_power in range (max_power, min_power, step_power):
-            print ("\nTX power ->",tx_power,"dBm")
 
-            # Send command to change power
-            change_power = "a-channel " + str(channel[ap_counter]) + " " + str(tx_power)
-            net_connect.send_command(change_power)
+def change_pwr_cmd(current_channel, current_tx_power):
+    return(f"a-channel {current_channel} {current_tx_power}")
 
-            # Send command to show current values and wait a second
-            output = net_connect.send_command(show_comand)
-            listed_output=output.split("\n")
-            for i in range (3):
-                print (listed_output[i])
-            time.sleep(1)
 
-        # Give some time to roam and revert power back to the max value
-        print ("\nReverting back to",max_power,"dBm in...")
-        for i in range (countdown, 0, -1):
-            print (i)
-            time.sleep(1)
-        print ("Now!")
-        print ("\nTX power ->",max_power,"dBm")
+# Function runs full round at one device
+def run_device(current_ap):
+    current_ap['username'] = username
+    current_ap['password'] = password
+
+    print("\n" + "====" * 12)
+    print("Connecting to AP", ap_counter, "...")
+    net_connect = ConnectHandler(**device)
+    print("Successful!")
+
+    # Change power - step by step
+    for tx_power in range(MAX_POWER, MIN_POWER, STEP_POWER):
+        print("\nTX power ->", tx_power, "dBm")
 
         # Send command to change power
-        change_power = "a-channel " + str(channel[ap_counter]) + " " + str(max_power)
+        change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(tx_power))
         net_connect.send_command(change_power)
 
-        # Send command to show current values
-        output = net_connect.send_command(show_comand)
-        listed_output=output.split("\n")
-        for i in range (3):
-            print (listed_output[i])
+        # Send command to show current values and wait a second
+        output = net_connect.send_command(show_cmd(SHOW_ARGS))
+        print(*output.split('\n')[0:3], sep='\n')
+        time.sleep(1)
 
-        # Setting counter for the next AP
-        ap_counter+=1
-    # One full round finishes here
+    # Give some time to roam and revert power back to the max value
+    print("\nReverting back to", MAX_POWER, "dBm in...")
+    for i in range(COUNTDOWN, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("Now!")
+    print("\nTX power ->", MAX_POWER, "dBm")
 
-print ("\nHope it was seamless...") #mheh
+    # Send command to change power
+    change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(MAX_POWER))
+    net_connect.send_command(change_power)
+
+    # Send command to show current values
+    output = net_connect.send_command(show_cmd(SHOW_ARGS))
+    print(*output.split('\n')[0:3], sep='\n')
+
+
+# It all starts here
+if __name__ == '__main__':
+    username = input("Username: ")
+    password = getpass.getpass()
+
+    # Get devices dictionary from file
+    with open('devices.json') as devices_file:
+        devices = json.load(devices_file)
+
+    print("\nIt's roaming time!")
+
+    # Replay as many times as REPLAY parameter dictates
+    for n in range(REPLAY):
+        print("\nRound", n+1)
+        ap_counter = 1
+
+        # Full round for each device
+        for device in devices:
+            run_device(device)
+            # Setting counter for the next AP
+            ap_counter += 1
+
+    print("\nHope it was seamless...")  # mheh
