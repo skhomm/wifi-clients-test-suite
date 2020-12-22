d7cd35aa576b72896d27494339c0f96e003be58c
skhomm <i.kwibus@gmail.com>
2020-12-22 09:21:56 +0300

    Program body is not separated by functions and variables anymore

--- make_roam/aruba/make_roam_aruba_IAP.py
+++ make_roam/aruba/make_roam_aruba_IAP.py
@@ -1,12 +1,15 @@
+"""
+This script creates conditions that drive a Wi-Fi client to roam.
+
+It's achieved by controlling power settings of APs.
+Client doesn't need to move. It roams from AP with low power.
+"""
+
 import time
 import json
 import getpass
 
 from netmiko import ConnectHandler
-
-# Ask for username and password
-username = input("Username: ")
-password = getpass.getpass()
 
 # Define parameters
 REPLAY = 2
@@ -15,18 +18,14 @@ MAX_POWER = 21
 STEP_POWER = -3
 COUNTDOWN = 10
 SHOW_ARGS = "EIRP|Channel|BSSID"
-CHANNEL = {1: 149, 2: 157}
-
-# Get devices dictionary from file
-with open('devices.json') as devices_file:
-    devices = json.load(devices_file)
+CHANNEL = {1: 149, 2: 154}
 
 
-def show_command(include_args):
+def show_cmd(include_args):
     return(f"show ap debug driver-config | inc {include_args}")
 
 
-def change_power_command(current_channel, current_tx_power):
+def change_pwr_cmd(current_channel, current_tx_power):
     return(f"a-channel {current_channel} {current_tx_power}")
 
 
@@ -45,11 +44,11 @@ def run_device(current_ap):
         print("\nTX power ->", tx_power, "dBm")
 
         # Send command to change power
-        change_power = change_power_command(str(CHANNEL[ap_counter]), str(tx_power))
+        change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(tx_power))
         net_connect.send_command(change_power)
 
         # Send command to show current values and wait a second
-        output = net_connect.send_command(show_command(SHOW_ARGS))
+        output = net_connect.send_command(show_cmd(SHOW_ARGS))
         print(*output.split('\n')[0:3], sep='\n')
         time.sleep(1)
 
@@ -62,17 +61,25 @@ def run_device(current_ap):
     print("\nTX power ->", MAX_POWER, "dBm")
 
     # Send command to change power
-    change_power = change_power_command(str(CHANNEL[ap_counter]), str(MAX_POWER))
+    change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(MAX_POWER))
     net_connect.send_command(change_power)
 
     # Send command to show current values
-    output = net_connect.send_command(show_command(SHOW_ARGS))
+    output = net_connect.send_command(show_cmd(SHOW_ARGS))
     print(*output.split('\n')[0:3], sep='\n')
 
 
 # It all starts here
+username = input("Username: ")
+password = getpass.getpass()
+
+# Get devices dictionary from file
+with open('devices.json') as devices_file:
+    devices = json.load(devices_file)
+
 print("\nIt's roaming time!")
 
+# Replay as many times as REPLAY parameter dictates
 for n in range(REPLAY):
     print("\nRound", n+1)
     ap_counter = 1
