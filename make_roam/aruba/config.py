62713b8a9e58f30a08c02884488f3ccd33968540
skhomm <62881270+skhomm@users.noreply.github.com>
2021-01-09 13:23:50 +0300

    Feature/main module (#2)
    * Refactor to make possible to run make_roam_aruba_IAP as external module
    
    * Add main module with menu to run submodules
    
    * Get rid of CHANNEL constant
    
    * Now all variables you may want to change live in config.py
    
    * Get rid of ap_counter
    
    * Put contents of devices.json into config.py
    
    * Add basic exceptions for SSH connection
    
    * Use absolute import, add setup.sh to work with :/Users/tekanov/projects/wifi-clients-test-suite
    
    Co-authored-by: skhomm <i.kwibus@gmail.com>

---
+++ make_roam/aruba/config.py
@@ -0,0 +1,27 @@
+"""
+Change this file.
+
+Do not change other files.
+"""
+
+REPLAY = 2
+MIN_POWER = 2
+MAX_POWER = 21
+STEP_POWER = -6
+COUNTDOWN = 5
+SHOW_ARGS = "EIRP|Channel|BSSID"
+
+DEVICES = [
+    {
+        "device_type":  "aruba_os",
+        "host":         "10.77.30.30",
+        "channel":      "149",
+        "ap_name":      "AP1"
+    },
+    {
+        "device_type":  "aruba_os",
+        "host":         "10.77.30.33",
+        "channel":      "153",
+        "ap_name":      "AP2"
+    }
+]
--- make_roam/aruba/make_roam_aruba_IAP.py
+++ make_roam/aruba/make_roam_aruba_IAP.py
@@ -5,90 +5,96 @@ It's achieved by controlling power settings of APs.
 Client doesn't need to move. It roams from AP with low power.
 """
 
+import sys
 import time
-import json
 import getpass
 
 from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import AuthenticationException
 
-# Define parameters
-REPLAY = 2
-MIN_POWER = 2
-MAX_POWER = 21
-STEP_POWER = -3
-COUNTDOWN = 10
-SHOW_ARGS = "EIRP|Channel|BSSID"
-CHANNEL = {1: 149, 2: 154}
+from make_roam.aruba import config
 
 
 def show_cmd(include_args):
     return(f"show ap debug driver-config | inc {include_args}")
 
 
-def change_pwr_cmd(current_channel, current_tx_power):
-    return(f"a-channel {current_channel} {current_tx_power}")
+def change_pwr_cmd(current_channel, current_power):
+    return(f"a-channel {current_channel} {current_power}")
 
 
 # Function runs full round at one device
-def run_device(current_ap):
-    current_ap['username'] = username
-    current_ap['password'] = password
+def run_device(device, username, password):
+    # Form dictionary that is supported by netmiko
+    netmiko_device = device.copy()
+    netmiko_device['username'] = username
+    netmiko_device['password'] = password
+
+    # Netmiko doesn't accept 'channel' in it's dictionary
+    channel = netmiko_device.pop('channel')
+    ap_name = netmiko_device.pop('ap_name')
+    host = netmiko_device['host']
 
     print("\n" + "====" * 12)
-    print("Connecting to AP", ap_counter, "...")
-    net_connect = ConnectHandler(**device)
-    print("Successful!")
+    print(f"Connecting to {ap_name} [{host}] ...")
+
+    try:
+        net_connect = ConnectHandler(**netmiko_device)
+        print("Successful!")
+    except AuthenticationException:
+        sys.exit("Authentication failed!")
+    except NetMikoTimeoutException:
+        sys.exit("TCP connection to the device failed!")
 
     # Change power - step by step
-    for tx_power in range(MAX_POWER, MIN_POWER, STEP_POWER):
-        print("\nTX power ->", tx_power, "dBm")
+    for power in range(config.MAX_POWER, config.MIN_POWER, config.STEP_POWER):
+        print(f"\nTX power -> {power} dBm")
 
         # Send command to change power
-        change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(tx_power))
+        change_power = change_pwr_cmd(str(channel), str(power))
         net_connect.send_command(change_power)
 
         # Send command to show current values and wait a second
-        output = net_connect.send_command(show_cmd(SHOW_ARGS))
+        output = net_connect.send_command(show_cmd(config.SHOW_ARGS))
         print(*output.split('\n')[0:3], sep='\n')
         time.sleep(1)
 
     # Give some time to roam and revert power back to the max value
-    print("\nReverting back to", MAX_POWER, "dBm in...")
-    for i in range(COUNTDOWN, 0, -1):
+    print(f"\nReverting back to {config.MAX_POWER} dBm in...")
+    for i in range(config.COUNTDOWN, 0, -1):
         print(i)
         time.sleep(1)
     print("Now!")
-    print("\nTX power ->", MAX_POWER, "dBm")
+    print(f"\nTX power -> {config.MAX_POWER} dBm")
 
     # Send command to change power
-    change_power = change_pwr_cmd(str(CHANNEL[ap_counter]), str(MAX_POWER))
+    change_power = change_pwr_cmd(str(channel), str(config.MAX_POWER))
     net_connect.send_command(change_power)
 
     # Send command to show current values
-    output = net_connect.send_command(show_cmd(SHOW_ARGS))
+    output = net_connect.send_command(show_cmd(config.SHOW_ARGS))
     print(*output.split('\n')[0:3], sep='\n')
 
 
-# It all starts here
-if __name__ == '__main__':
+def main():
     username = input("Username: ")
     password = getpass.getpass()
-
-    # Get devices dictionary from file
-    with open('devices.json') as devices_file:
-        devices = json.load(devices_file)
 
     print("\nIt's roaming time!")
 
     # Replay as many times as REPLAY parameter dictates
-    for n in range(REPLAY):
+    for n in range(config.REPLAY):
+
         print("\nRound", n+1)
-        ap_counter = 1
 
         # Full round for each device
-        for device in devices:
-            run_device(device)
-            # Setting counter for the next AP
-            ap_counter += 1
+        for device in config.DEVICES:
+            run_device(device, username, password)
 
     print("\nHope it was seamless...")  # mheh
+
+
+# It all starts here
+if __name__ == '__main__':
+    main()
