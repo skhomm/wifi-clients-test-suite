99a95c19075989fd6dd65bd044cb5d68ad2ba644
ttl256 <64251319+ttl256@users.noreply.github.com>
2021-01-15 18:45:21 +0300

    Trying out

--- client_capabilities/assoc_req.py
+++ client_capabilities/assoc_req.py
@@ -4,13 +4,18 @@ from scapy.all import *
 # Can be done using a function from module that sets
 # adapter to a monitor mode.
 
-#def assoc_req(frame):
-    #if frame.haslayer(Dot11AssoReq):
-        #print('Association Request')
-
 def assoc_req_parse(frame):
     print(frame.addr2)
 
-sniff(iface='wlan0', lfilter = lambda x: x.haslayer(Dot11AssoReq), prn=assoc_req_parse, store=0)
+#sniff(iface='wlan0', lfilter = lambda x: x.haslayer(Dot11AssoReq), prn=assoc_req_parse, store=0)
 #s = AsyncSniffer(iface="wlan0", lfilter = lambda x: x.haslayer(Dot11AssoReq), prn=assoc_req_parse)
+
+def sniff(interface, func):
+    sniff(iface=interface, prn=func, store=0)
+
+def main():
+    sniff(wlan0, assoc_req_parse)
+
+if __name__ == '__main__':
+    main()
 
