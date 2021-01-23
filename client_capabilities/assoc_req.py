af7ab50accc0f01315653a5107251e6d73ad829e
ttl256 <64251319+ttl256@users.noreply.github.com>
2021-01-23 17:34:57 +0300

    Handle exceptions (supported channels weren't advertised)

--- client_capabilities/assoc_req.py
+++ client_capabilities/assoc_req.py
@@ -8,24 +8,27 @@ INTERFACE = "wlan0"
 SSID = "lab-iap"
 
 def supported_channels_parse(frame):
-    # Convert info field of the IE (b'' string) to hex
-    channels_raw = frame.getlayer(Dot11Elt, ID=36).info.hex()
     supported_channels = []
-    # IEEE802.11 defines the following format for supported channels:
-    # 1 byte (2 hex digits) for 'first channel number'
-    # 1 byte (2 hex digits) for 'number of channels'
-    for i in range(0, len(channels_raw), 4):
-        tmp = channels_raw[i:i+4]
-        first_channel = int(tmp[:2], 16)
-        num_of_channels = int(tmp[2:], 16)
-        supported_channels.extend(
-            [first_channel+num_of_channels*i for i in range(0, num_of_channels)])
     
-    return supported_channels    
+    try:
+        # Convert info field of the IE (b'' string) to hex
+        channels_raw = frame.getlayer(Dot11Elt, ID=36).info.hex()
+        # IEEE802.11 defines the following format for supported channels:
+        # 1 byte (2 hex digits) for 'first channel number'
+        # 1 byte (2 hex digits) for 'number of channels'
+        for i in range(0, len(channels_raw), 4):
+            tmp = channels_raw[i:i+4]
+            first_channel = int(tmp[:2], 16)
+            num_of_channels = int(tmp[2:], 16)
+            supported_channels.extend(
+                [first_channel+num_of_channels*i for i in range(0, num_of_channels)])
+
+        return supported_channels
+    except AttributeError:
+        # Return an empty list of supported channels if a client doesn't advertise them
+        return supported_channels   
 
 def assoc_req_parse(frame):
-    if (frame.haslayer(Dot11AssoReq) and 
-        frame.getlayer(Dot11Elt, ID=0).info.decode("utf-8") == SSID):
         client_mac = frame.addr2
         bssid = frame.addr3
         
@@ -33,7 +36,11 @@ def assoc_req_parse(frame):
 
         print(f"Client {client_mac} sent Assoc Req to BSSID {bssid}")
         print("Supported channels")
-        print(*supported_channels, sep = ", ")
+
+        if not supported_channels:
+            print("No info")
+        else:
+            print(*supported_channels, sep = ", ")
 
 def main():
     print("####Catching Association Request####")
