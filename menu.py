75565a6875d8c36e8b2d137b7aa89832210d08d5
skhomm <i.kwibus@gmail.com>
2021-03-06 17:59:06 +0300

    Add assoc_req option to menu

--- menu.py
+++ menu.py
@@ -10,6 +10,7 @@ import time
 
 import adapter_control
 from make_roam.aruba import iap
+from client_capabilities import assoc_req
 
 
 def menu():
@@ -52,9 +53,22 @@ def option_1():
 
 
 def option_2():
-    print("\nCurrently not supported...\n")
-    time.sleep(1)
+    print("\nSelect mode")
+    print("[1] Online")
+    print("[2] Offline")
 
+    mode_chosen = input("\nType number and press Enter\n")
+
+    if mode_chosen == "1":
+        print("\nStarting capture...\n")
+        assoc_req.main()
+    elif mode_chosen == "2":
+        print("\nCurrently not supported...\n")
+    elif mode_chosen == "  ":
+        return
+    else:
+        print("\nInput not recognized")
+        time.sleep(1)
 
 def option_3():
     print("\nSelect vendor")
@@ -68,8 +82,11 @@ def option_3():
         iap.main()
     elif vendor_chosen == "2":
         print("\nCurrently not supported...\n")
+    elif vendor_chosen == "  ":
+        return
     else:
         print("\nInput not recognized")
+        time.sleep(1)
 
 
 menu()
